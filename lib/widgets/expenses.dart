cd4ff407976b378839d932178c4c68060f38bb10
vikram <vkbiotech841@gmail.com>
2025-09-15 17:02:47 +0530

    media query added for the responsive design

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -31,6 +31,8 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea:
+          true, // to avoid overlapping with the camera or safe area in the mobile
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(
@@ -72,6 +74,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -93,12 +97,19 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
